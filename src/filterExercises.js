9b13bb7153b099a36ac9c45f4802a15cab52c5d9
tessema2021 <ninafuchere@gmail.com>
2021-11-11 16:09:23 -0600

    fifth pass

--- src/filterExercises.js
+++ src/filterExercises.js
@@ -20,12 +20,19 @@ console.log(getFullTimeStudents());
 // Export a function called getStudentsByInstructorId
 // It should accept one integer parameter name `instructorId`
 // It should return an array of students with that instructor
-
+export const getStudentsByInstructorId = () => {
+    return students.filter(student => student.instructorId === instructorId)
+}
 // Export a function called getPolyglotStudents
 // It should accept one integer parameter named `languageCount`
 // It should return an array of students who know as many (or more) languages than `languageCount`
 // Ex: If the number 2 is passed to the function, only the students who know 2 or more languages should be returned
-
+export const getPolyglotStudents = (languageCount) => {
+    const studentWithMultipleLanguage = students.filter(
+        (students) => students.languages.length >= languageCount
+    )
+    return studentWithMultipleLanguage;
+}
 // Export a function called getAvailableInstructors
 // It should not accept any parameters
 // It should return an array of instructors that don't have any students
